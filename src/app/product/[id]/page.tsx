4baf6a97e202d35607e7790cbb573b8de4601845
ThauanyAA <thauanyaraujo5@gmail.com>
2025-08-23 16:17:43 -0300

    Fetching product by id

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,28 +1,51 @@
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+import Image from "next/image";
 
 interface ProductPageProps {
-  params: {
-    id: string;
-  };
+  params: { id: string }
 }
 
-export default function ProductPage({ params }: ProductPageProps) {
+export const revalidate = 60 * 60 * 1 // 1 hora
+
+export async function generateStaticParams() {
+  const response = await stripe.products.list()
+  return response.data.map((product) => ({ id: product.id }))
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const productId = params.id;
+
+  const response = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  });
+
+  const price = response.default_price as Stripe.Price;
+
+  const product = {
+    id: response.id,
+    name: response.name,
+    imageUrl: response.images[0],
+    price: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format((price.unit_amount ?? 0) / 100),
+    description: response.description
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
-
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores aliquid rerum exercitationem facere a molestiae ut sed velit non mollitia? Officiis hic velit assumenda aspernatur nihil, sint sed laboriosam tempora?</p>
-
-        <button>
-          Comprar agora
-        </button>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+        <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
-
